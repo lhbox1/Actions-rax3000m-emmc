117d34b4bc597e1b5f2b131ddc40d82435244b32
lhbox1 <123519621+lhbox1@users.noreply.github.com>
2024-03-17 03:06:53 +0800

    Update diy.sh

--- scripts/diy.sh
+++ scripts/diy.sh
@@ -2,8 +2,3 @@
 
 #更改默认地址为192.168.6.1
 sed -i 's/192.168.1.1/192.168.15.1/g' package/base-files/files/bin/config_generate
-
-
-
-
-git clone https://github.com/kenzok8/openwrt-packages.git package/openwrt-packages
