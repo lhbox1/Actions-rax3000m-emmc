aee746271d59e77bdbbcd1c8f351c21995322af0
lhbox1 <123519621+lhbox1@users.noreply.github.com>
2024-03-28 14:35:14 +0800

    Update diy.sh

--- scripts/diy.sh
+++ scripts/diy.sh
@@ -2,3 +2,6 @@
 
 #更改默认地址为192.168.6.1
 sed -i 's/192.168.1.1/192.168.15.1/g' package/base-files/files/bin/config_generate
+
+##添加软件包
+git clone https://github.com/lhbox1/luci-app-adguardhome package/luci-app-adguardhome
