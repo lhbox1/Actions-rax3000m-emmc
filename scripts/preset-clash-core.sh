d052dce95519a5ad989360ee831bc17a68b10b6b
lhbox1 <123519621+lhbox1@users.noreply.github.com>
2024-04-01 13:31:35 +0800

    Update preset-clash-core.sh

--- scripts/preset-clash-core.sh
+++ scripts/preset-clash-core.sh
@@ -82,12 +82,12 @@ wget -qO- $COUNTRY_LITE_URL > files/etc/openclash/Country.mmdb
 #wget -qO- $chnr6_custom_url > files/etc/openclash/china_ip6_route.ipset
 #wget -qO- $cndomain_custom_url > files/etc/openclash/accelerated-domains.china.conf
 
-#下载使用master版本openclash
-#wget -qO- https://github.com/vernesong/OpenClash/archive/master.zip > temp.zip
-#unzip -q temp.zip
-#cp -r OpenClash-master/luci-app-openclash package/
-#rm -rf OpenClash-master
-#rm -rf temp.zip
+下载使用master版本openclash
+wget -qO- https://github.com/vernesong/OpenClash/archive/master.zip > temp.zip
+unzip -q temp.zip
+cp -r OpenClash-master/luci-app-openclash package/
+rm -rf OpenClash-master
+rm -rf temp.zip
 
 #下载使用master版本passwall
 wget -qO- https://github.com/xiaorouji/openwrt-passwall/archive/main.zip > temp1.zip
@@ -170,8 +170,8 @@ rm -rf luci-app-mosdns-5
 
 
 #下载使用dev版本openclash
-wget  -qO-  wget https://codeload.github.com/vernesong/OpenClash/zip/refs/heads/dev > temp.zip
-unzip -q temp.zip
-cp -r OpenClash-dev/luci-app-openclash package/
-rm -rf OpenClash-dev
-rm -rf temp.zip
+#wget  -qO-  wget https://codeload.github.com/vernesong/OpenClash/zip/refs/heads/dev > temp.zip
+#unzip -q temp.zip
+#cp -r OpenClash-dev/luci-app-openclash package/
+#rm -rf OpenClash-dev
+#rm -rf temp.zip
