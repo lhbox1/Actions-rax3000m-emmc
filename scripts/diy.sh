14ee863c980f7067ffefae5c47dbf6c5375c6bfa
lhbox1 <123519621+lhbox1@users.noreply.github.com>
2024-04-01 16:50:55 +0800

    Update diy.sh

--- scripts/diy.sh
+++ scripts/diy.sh
@@ -3,6 +3,22 @@
 #更改默认地址为192.168.6.1
 sed -i 's/192.168.1.1/192.168.15.1/g' package/base-files/files/bin/config_generate
 
+#删除冲突插件
+#rm -rf $(find ./feeds/luci/ -type d -regex ".*\(argon\|design\|openclash\|passwall\|ssr-plus\).*")
+rm -rf $(find ./feeds/luci/ -type d -regex ".*\(openclash\|passwall\|ssr-plus\|easymesh\).*")
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 20.x feeds/packages/lang/golang
+#修改默认主机名
+sed -i "s/hostname='.*'/hostname='RAX3000M_E'/g" package/base-files/files/bin/config_generate
+
+##-----------------Delete DDNS's examples-----------------
+sed -i '/myddns_ipv4/,$d' feeds/packages/net/ddns-scripts/files/etc/config/ddns
+##-----------------Manually set CPU frequency for MT7981B-----------------
+sed -i '/"mediatek"\/\*|\"mvebu"\/\*/{n; s/.*/\tcpu_freq="1.3GHz" ;;/}' package/emortal/autocore/files/generic/cpuinfo
+
+
+
+
 ##添加软件包
 git clone https://github.com/messense/aliyundrive-webdav package/messense
 
