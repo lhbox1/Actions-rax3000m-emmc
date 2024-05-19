345611d31f7be7c35dc92b1a5941f8105cffdbca
lhbox1 <123519621+lhbox1@users.noreply.github.com>
2024-05-20 07:01:08 +0800

    Update preset-clash-core.sh

--- scripts/preset-clash-core.sh
+++ scripts/preset-clash-core.sh
@@ -90,12 +90,13 @@ rm -rf OpenClash-master
 rm -rf temp.zip
 
 #下载使用master版本passwall
-wget -qO- https://github.com/xiaorouji/openwrt-passwall/archive/main.zip > temp1.zip
+#wget -qO- https://github.com/xiaorouji/openwrt-passwall/archive/main.zip > temp1.zip
+wget -qO- https://codeload.github.com/xiaorouji/openwrt-passwall/zip/refs/tags/4.76-5 > temp1.zip
 unzip -q temp1.zip
 rm -rf feeds/luci/applications/luci-app-passwall
-cp -r openwrt-passwall-main/luci-app-passwall package/
+cp -r openwrt-passwall-4.76-5/luci-app-passwall package/
 rm -rf temp1.zip
-rm -rf openwrt-passwall-main
+rm -rf openwrt-passwall-4.76-5
 
 
 
